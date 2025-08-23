d0294e96456ef0f62438006ba647981df8cc5059
rahulkpr2510 <rahulkpr2510@gmail.com>
2025-08-23 12:48:42 +0530

    fix: remove redundant afterSignOutUrl prop from UserButton

--- components/header.jsx
+++ components/header.jsx
@@ -97,7 +97,6 @@ const Header = async () => {
                   userPreviewMainIdentifier: "font-semibold",
                 },
               }}
-              afterSignOutUrl="/"
             />
           </SignedIn>
         </div>
